08e315356ed25b37274aa3f8d88d4b0130e6b4d7
alexandre_driaguine <alexandre.driaguine@konsult.atg.se>
2020-05-04 20:08:17 +0200

    change hideous render props to beautiful hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import * as React from 'react'
 import styled from '@emotion/styled'
 import { Global } from '@emotion/core'
 import { globalStyle } from './globalStyles'
-import { CharacterRoutes } from './characters/character-routes'
-import { Switch, Route, Link, BrowserRouter, Redirect } from 'react-router-dom'
+import {
+  Switch,
+  Route,
+  Link,
+  BrowserRouter,
+  useLocation,
+} from 'react-router-dom'
 import { Home } from './home'
 import { Header } from './header'
-import { Transition, animated, config } from 'react-spring/renderprops'
+import { useTransition, animated } from 'react-spring'
 import { Characters } from './characters/characters-view'
 import { CharacterDetails } from './characters/character-details-view'
 
@@ -17,68 +22,47 @@ const Main = styled.main`
   margin: 0 auto;
 `
 
-const Fill = styled.div`
-  top: 64px;
-  position: absolute;
-  width: 100%;
-  max-width: 760px;
-`
-
 export const App = () => {
+  const location = useLocation()
+  const transitions = useTransition(location, (location) => location.pathname, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  })
+
+  React.useEffect(() => {
+    console.log(location.pathname)
+    window.scrollTo(0, 0)
+  }, [location.pathname])
+
   return (
     <>
-      <BrowserRouter>
-        <Route
-          render={(props) => {
-            return (
-              <Main>
-                <Header />
-                <Transition
-                  keys={props.location.pathname.split('/')[1]}
-                  from={{ opacity: 0, transform: 'translateY(100%)' }}
-                  enter={{ opacity: 1, transform: 'translateY(0)' }}
-                  leave={{ opacity: 0, transform: 'translateY(100%)' }}
-                  items={props.location}
-                >
-                  {(loc, state) => (style) => {
-                    return (
-                      <Switch
-                        location={state === 'update' ? props.location : loc}
-                      >
-                        <Route component={Home} exact path="/" />
-                        <Route
-                          render={() => (
-                            <Fill>
-                              <animated.div style={{ ...style }}>
-                                <Characters />
-                              </animated.div>
-                            </Fill>
-                          )}
-                          path="/characters"
-                        />
-                        <Route
-                          render={() => (
-                            <Fill>
-                              <animated.div style={{ ...style }}>
-                                <CharacterDetails />
-                              </animated.div>
-                            </Fill>
-                          )}
-                          path="/character/:id"
-                        />
-                        <Route path="*">
-                          nah not found
-                          <Link to="/">Go to home</Link>
-                        </Route>
-                      </Switch>
-                    )
-                  }}
-                </Transition>
-              </Main>
-            )
-          }}
-        />
-      </BrowserRouter>
+      <Main>
+        <Header />
+        {transitions.map(({ item: location, props, key }) => (
+          <animated.div
+            key={key}
+            style={{
+              ...props,
+              position: 'absolute',
+              top: 64,
+              width: '100%',
+              maxWidth: 760,
+              margin: '0 auto',
+            }}
+          >
+            <Switch location={location}>
+              <Route component={Home} exact path="/" />
+              <Route component={Characters} exact path="/characters" />
+              <Route component={CharacterDetails} path="/characters/:id" />
+              <Route path="*">
+                nah not found
+                <Link to="/">Go to home</Link>
+              </Route>
+            </Switch>
+          </animated.div>
+        ))}
+      </Main>
       <Global styles={globalStyle} />
     </>
   )
